d475e63255e991226988054053a743d6c6174b12
Abdul Fatah <fatah03111995@gmail.com>
2025-09-01 10:12:10 +0700

    update(User): change button

--- app/Filament/Resources/Users/Pages/CreateUser.php
+++ app/Filament/Resources/Users/Pages/CreateUser.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Resources\Users\Pages;
 
 use App\Filament\Resources\Users\UserResource;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateUser extends CreateRecord
@@ -13,5 +14,25 @@ class CreateUser extends CreateRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    //form action
+    protected function getFormActions(): array
+    {
+        return [
+            Action::make('Save')
+                ->submit('Save')
+                ->label('Create User')
+                ->color('primary')
+                ->size('lg')
+                ->action(function () {
+                    $this->save();
+                }),
+            Action::make('Cancel')
+                ->label('Cancel')
+                ->color('secondary')
+                ->size('lg')
+                ->url($this->getResource()::getUrl('index')),
+        ];
     }
 }
