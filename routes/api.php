2975750032c4c16cbd1cbbb819107540cfe7e2eb
Abdul Fatah <fatah03111995@gmail.com>
2025-09-03 16:18:57 +0700

    update(api): adding login route

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/login', [\App\Http\Controllers\api\AuthController::class, 'login']);
+//post adalah methode statis dari class Route
+//parameter pertama adalah endpoint
+//parameter kedua adalah handler yang digunakan
+//handler berupa clossur(di dart anonymous function atau lambda), callable array
+//Callable array adalah array yang berisi dua elemen
+// elemen pertama adalah nama class, elemen kedua adalah nama method
+
+//Menggunakan clossure, hanya untuk sekala kecil
+// Route::post('/login', function(){
+//     $controller = new \App\Http\Controllers\api\AuthController();
+//     return $controller->login(request());
+// });
