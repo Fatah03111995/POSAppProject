1b5a235c506fed58c124b49abfa64fa725f4bc7d
Abdul Fatah <fatah03111995@gmail.com>
2025-09-03 15:29:36 +0700

    feat: auth controller

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    //Login
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email'=>'required|email', // apakah email memiliki format email
+            'password'=>'required|string', // apakah password berupa string
+        ]);
+
+        //Memastikan apakah user dengan email yang dikirm terdaftar
+        $user = \App\Models\User::where('email', $credentials['email'])->first();
+
+        //Jika user ridak ditemukan, kembalikan response error
+        if(!$user){
+            return response()->json(
+                [
+                    'message'=>'User not found',
+                ], 404
+                );
+        }
+
+        //Jika user ditemukan, periksa apakah password sesuai, jika tidak sesuai kembalikan response error
+        if(!Hash::check($credentials['password'], $user->password))
+        {
+            return response()->json(
+                [
+                    'message'=>'Invalid Credentials',
+                ], 401
+            );
+        }
+
+        //Jika email dan password sesuai, buat token baru
+        $token = $user->createToken('auth_token')->plainTextToken;
+        //createToken adalah method dari trait HasApiTokens
+        //plainTextToken adalah token dalam bentuk string
+
+        //Kembalikan response berisi data user dan token
+        return response()->json(
+            [
+                'message'=>'Login Success',
+                'token'=>$token,
+                'user'=>$user,
+            ], 200
+        );
+    }
+}
